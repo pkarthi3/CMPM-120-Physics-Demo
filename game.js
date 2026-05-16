7e1f05feb7fa5b2bcc37e88cf7d729d4eaa135d4
Pavithra Karthikeyan <pkarthi3@ucsc.edu>
2026-05-16 01:16:19 -0700

    added (unfinished) goal object, added means of respawning the player upon falling offscreen

--- game.js
+++ game.js
@@ -13,15 +13,18 @@ class Level1 extends Phaser.Scene {
         this.player = this.physics.add.image(100, 100, 'frog');
         this.player.setScale(0.05);
         this.lilypads = this.physics.add.staticGroup();
-        this.lilypads.create(100, 400, 'lilypad').setScale(0.1).refreshBody();
-        this.lilypads.create(300, 300, 'lilypad').setScale(0.1).refreshBody();
-        this.lilypads.create(400, 200, 'lilypad').setScale(0.1).refreshBody();
-        
+        this.lilypads.create(100, 550, 'lilypad').setScale(0.1).refreshBody();
+        this.lilypads.create(300, 400, 'lilypad').setScale(0.1).refreshBody();
+        this.lilypads.create(400, 250, 'lilypad').setScale(0.1).refreshBody();
+        this.goal = this.physics.add.image(500, 0, 'goal');
+        this.goal.setScale(0.1);
+        this.goal.body.allowGravity = false;
 
         this.physics.add.collider(this.player, this.lilypads);
 
         //based on example solution on labs.phaser.io
         this.cursors = this.input.keyboard.createCursorKeys();
+
     }
     update() {
 
@@ -37,7 +40,13 @@ class Level1 extends Phaser.Scene {
             this.player.setVelocityX(0);
         }
         if (up.isDown && this.player.body.touching.down) {
-            this.player.setVelocityY(-200);
+            this.player.setVelocityY(-300);
+        }
+
+        if (this.player.body.y > 700) {
+            this.player.body.x = 100;
+            this.player.body.y = 450;
+            this.player.setVelocityY(0);
         }
     }
 }
