704b2de2110a89112269dbdc3b212f2152273b75
Pavithra Karthikeyan <pkarthi3@ucsc.edu>
2026-05-18 13:54:09 -0700

    finished level 2, added placeholder for level 3

--- game.js
+++ game.js
@@ -15,6 +15,8 @@ class Level1 extends Phaser.Scene {
         this.endTime = 0;
         this.prevTime = this.time.now;
 
+        this.scene.start('level3');
+
         this.player = this.physics.add.image(400, 400, 'frog');
         this.player.setScale(0.05);
 
@@ -59,9 +61,9 @@ class Level1 extends Phaser.Scene {
         });
         
         //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
-        this.physics.add.collider(this.player, this.coins, this.collectCoin, null, this);
+        this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
 
-        this.add.text(15, 15, 'use left/right arrow keys to move and up to jump; try to reach the goal marked with the flag while collecting as many coins as you can', { wordWrap: { width: 300 }}).setColor('#000000');
+        this.add.text(15, 15, 'Use left/right arrow keys to move and up to jump; try to reach the goal marked with the flag while collecting as many coins as you can', { wordWrap: { width: 300 }}).setColor('#000000');
         //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cevents%5Clisten%20to%20game%20object%20event.js&return=phaser4-index.html%3Fpath%3Devents
         this.scoreText = this.add.text(15, 100, '').setColor('#000000');
 
@@ -124,8 +126,290 @@ class Results1 extends Phaser.Scene {
             this.scene.start('level1');
         });
         this.next = this.add.text(500, 500, 'Next Level', {fontSize: 40}).setColor('#000000');
+        this.next.setInteractive();
+        this.next.on('pointerdown', () => {
+            this.scene.start('level2');
+        });
     }
     update() {}
+}
+
+class Level2 extends Phaser.Scene {
+    constructor() {
+        super('level2');
+    }
+    preload() {
+        this.load.path = 'assets/';
+        this.load.image('frog', 'frog.png');
+        this.load.image('coin', 'coin.png');
+        this.load.image('lilypad', 'lilypad.png');
+        this.load.image('goal', 'lilypadgoal.png');
+        this.load.image('cloud', 'cloud.png');
+    }
+    create() {
+        score = 0;
+        this.endTime = 0;
+        this.prevTime = this.time.now;
+
+        this.player = this.physics.add.image(100, 500, 'frog');
+        this.player.setScale(0.025);
+
+        this.lilypads = this.physics.add.staticGroup();
+        this.lilypads.create(100, 550, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(450, 250, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(600, 350, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(150, 150, 'lilypad').setScale(0.05).refreshBody();
+
+        this.coins = this.physics.add.staticGroup();
+        this.coins.create(500, 300, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(200, 150, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(600, 300, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(400, 325, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(150, 125, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(400, 200, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(300, 100, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(450, 150, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(650, 325, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(600, 50, 'coin').setScale(0.01).refreshBody();
+
+        this.clouds = this.physics.add.staticGroup();
+        this.clouds.create(200, 400, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(400, 500, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(575, 150, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(325, 300, 'cloud').setScale(0.05).refreshBody();
+
+
+        this.goal = this.physics.add.image(400, 50, 'goal');
+        this.goal.setScale(0.05);
+        this.goal.body.setSize(this.goal.body.width, this.goal.body.halfHeight);
+        this.goal.body.setOffset(0, this.goal.body.height);
+        this.goal.body.allowGravity = false;
+        this.goal.body.setImmovable(true);
+
+        this.physics.add.collider(this.player, this.lilypads);
+        this.physics.add.collider(this.player, this.goal, () => {
+            if (this.player.body.touching.down && this.player.body.y < this.goal.body.y) {
+                //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cscenes%5Cpassing%20data%20to%20a%20scene.js&return=phaser4-index.html%3Fpath%3Dscenes
+                this.scene.start('results2', { endTime: this.endTime});
+            }
+        });
+        
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
+
+        this.physics.add.collider(this.player, this.clouds, this.cloudVanish, null, this);
+
+        this.add.text(15, 15, 'The clouds aren\'t as solid as they may look...', { wordWrap: { width: 300 }}).setColor('#000000');
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cevents%5Clisten%20to%20game%20object%20event.js&return=phaser4-index.html%3Fpath%3Devents
+        this.scoreText = this.add.text(15, 100, '').setColor('#000000');
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    }
+    update() {
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        const { left, right, up } = this.cursors;
+        if (left.isDown) {
+            this.player.setVelocityX(-100);
+        }
+        else if (right.isDown) {
+            this.player.setVelocityX(100);
+        }
+        else {
+            this.player.setVelocityX(0);
+        }
+        if (up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-300);
+        }
+
+        if (this.player.body.y > 700) {
+            this.player.body.x = 100;
+            this.player.body.y = 500;
+            this.player.setVelocityY(0);
+            //based on example solution found at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+            for ( let cloud of this.clouds.getChildren()) {
+                cloud.enableBody(true, cloud.x, cloud.y, true, true);
+            }
+        }
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cevents%5Clisten%20to%20game%20object%20event.js&return=phaser4-index.html%3Fpath%3Devents
+        this.scoreText.setText('score: ' + score);
+        this.endTime = this.time.now - this.prevTime;
+
+    }
+
+    //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+    collectCoin(player, coin){
+        coin.disableBody(true, true);
+        score++;
+    }
+
+    cloudVanish(player, cloud){
+        if (player.body.touching.down && player.body.y < cloud.body.y) {
+            cloud.disableBody(true, true);
+        }
+    }
+}
+
+class Results2 extends Phaser.Scene {
+    constructor() {
+        super('results2');
+    }
+    //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cscenes%5Cpassing%20data%20to%20a%20scene.js&return=phaser4-index.html%3Fpath%3Dscenes
+    init(data) {
+        this.finishTime = data.endTime;
+    }
+    preload() {}
+    create() {
+        this.add.text(100, 100, 'Level Complete!', {fontSize: 40}).setColor('#000000');
+        this.add.text(100, 150, 'Time: ' + this.finishTime/1000 + ' seconds', {fontSize: 40}).setColor('#000000');
+        this.add.text(100, 200, 'Score: ' + score, {fontSize: 40}).setColor('#000000');
+        this.retry = this.add.text(100, 500, 'Retry', {fontSize: 40}).setColor('#000000');
+        this.retry.setInteractive();
+        this.retry.on('pointerdown', () => {
+            this.scene.start('level2');
+        });
+        this.next = this.add.text(500, 500, 'Next Level', {fontSize: 40}).setColor('#000000');
+        this.next.setInteractive();
+        this.next.on('pointerdown', () => {
+            this.scene.start('level3');
+        })
+    }
+    update() {}
+}
+
+class Level3 extends Phaser.Scene {
+    constructor() {
+        super('level3');
+    }
+    preload() {
+        this.load.path = 'assets/';
+        this.load.image('frog', 'frog.png');
+        this.load.image('coin', 'coin.png');
+        this.load.image('lilypad', 'lilypad.png');
+        this.load.image('goal', 'lilypadgoal.png');
+        this.load.image('cloud', 'cloud.png');
+        this.load.image('trap', 'lilypadtrap.png');
+    }
+    create() {
+        score = 0;
+        this.endTime = 0;
+        this.prevTime = this.time.now;
+
+        this.player = this.physics.add.image(100, 500, 'frog');
+        this.player.setScale(0.025);
+
+        this.lilypads = this.physics.add.staticGroup();
+        this.lilypads.create(100, 550, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(450, 250, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(600, 350, 'lilypad').setScale(0.05).refreshBody();
+        this.lilypads.create(150, 150, 'lilypad').setScale(0.05).refreshBody();
+
+        this.traps = this.physics.add.staticGroup();
+
+        this.coins = this.physics.add.staticGroup();
+        this.coins.create(500, 300, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(200, 150, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(600, 300, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(400, 325, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(150, 125, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(400, 200, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(300, 100, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(450, 150, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(650, 325, 'coin').setScale(0.01).refreshBody();
+        this.coins.create(600, 50, 'coin').setScale(0.01).refreshBody();
+
+        this.clouds = this.physics.add.staticGroup();
+        this.clouds.create(200, 400, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(400, 500, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(575, 150, 'cloud').setScale(0.05).refreshBody();
+        this.clouds.create(325, 300, 'cloud').setScale(0.05).refreshBody();
+
+
+        this.goal = this.physics.add.image(400, 50, 'goal');
+        this.goal.setScale(0.05);
+        this.goal.body.setSize(this.goal.body.width, this.goal.body.halfHeight);
+        this.goal.body.setOffset(0, this.goal.body.height);
+        this.goal.body.allowGravity = false;
+        this.goal.body.setImmovable(true);
+
+        this.physics.add.collider(this.player, this.lilypads);
+        this.physics.add.collider(this.player, this.goal, () => {
+            if (this.player.body.touching.down && this.player.body.y < this.goal.body.y) {
+                //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cscenes%5Cpassing%20data%20to%20a%20scene.js&return=phaser4-index.html%3Fpath%3Dscenes
+                this.scene.start('results2', { endTime: this.endTime});
+            }
+        });
+
+        this.physics.add.collider(this.player, this.traps, () => {
+            this.player.body.x = 100;
+            this.player.body.y = 500;
+            this.player.setVelocityY(0);
+            //based on example solution found at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+            for ( let cloud of this.clouds.getChildren()) {
+                cloud.enableBody(true, cloud.x, cloud.y, true, true);
+            }
+        });
+        
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
+
+        this.physics.add.collider(this.player, this.clouds, this.cloudVanish, null, this);
+
+        this.add.text(15, 15, 'Watch out for the traps!', { wordWrap: { width: 300 }}).setColor('#000000');
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cevents%5Clisten%20to%20game%20object%20event.js&return=phaser4-index.html%3Fpath%3Devents
+        this.scoreText = this.add.text(15, 100, '').setColor('#000000');
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    }
+    update() {
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+        const { left, right, up } = this.cursors;
+        if (left.isDown) {
+            this.player.setVelocityX(-100);
+        }
+        else if (right.isDown) {
+            this.player.setVelocityX(100);
+        }
+        else {
+            this.player.setVelocityX(0);
+        }
+        if (up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-300);
+        }
+
+        if (this.player.body.y > 700) {
+            this.player.body.x = 100;
+            this.player.body.y = 500;
+            this.player.setVelocityY(0);
+            //based on example solution found at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+            for ( let cloud of this.clouds.getChildren()) {
+                cloud.enableBody(true, cloud.x, cloud.y, true, true);
+            }
+        }
+
+        //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src%5Cevents%5Clisten%20to%20game%20object%20event.js&return=phaser4-index.html%3Fpath%3Devents
+        this.scoreText.setText('score: ' + score);
+        this.endTime = this.time.now - this.prevTime;
+
+    }
+
+    //based on example solution at https://labs.phaser.io/phaser4-view.html?src=src\physics\arcade\basic%20platform.js&return=phaser4-index.html?path=physics/arcade
+    collectCoin(player, coin){
+        coin.disableBody(true, true);
+        score++;
+    }
+
+    cloudVanish(player, cloud){
+        if (player.body.touching.down && player.body.y < cloud.body.y) {
+            cloud.disableBody(true, true);
+        }
+    }
 }
 
 let config = {
@@ -140,7 +424,7 @@ let config = {
             debug: true,
         }
     },
-    scene: [ Level1, Results1 ]
+    scene: [ Level1, Results1, Level2, Results2, Level3 ]
 }
 
 let game = new Phaser.Game(config);
